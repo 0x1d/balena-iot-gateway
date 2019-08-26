83e3d25661798e16c92484936e1428a4e825b329
Patrick Balsiger <patrick@brudi.com>
2019-08-26 10:51:40 +0200

    keep script running with whle loop

--- zerotier/entrypoint.sh
+++ zerotier/entrypoint.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-# FIXME this should be started background through an init system
 echo "Start ZeroTier Daemon"
-nohup zerotier-one &
-sleep 10
+zerotier-one -d
+sleep 10 # not cool
 zerotier-cli info
 
 echo "Joining netwok ${ZT_NW_ID}"
-zerotier-cli join ${ZT_NW_ID}
+ZT_STATUS=$(echo $(zerotier-cli join ${ZT_NW_ID} | awk '{print $3}'))
+
+if [ "$ZT_STATUS" = "OK" ]; then 
+    echo "Join successful." 
+else
+    echo "Join unsuccessful. Exit." 
+    exit 0
+fi;
+
+while ((  ZT_STATUS == 'ONLINE' ))
+do
+    ZT_STATUS=$(echo $(zerotier-cli info) | awk '{print $5}')
+	sleep 5
+done
